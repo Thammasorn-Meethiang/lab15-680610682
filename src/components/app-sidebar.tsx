a151fae5df79b43d4b379e82d8db089a92d16a51
Pleum <thammasorn_m@cmu.ac.th>
2026-09-23 18:19:51 +0700

    app side bar fix

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -15,7 +15,6 @@ import {
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import { Separator } from "@/components/ui/separator";
 import { currentUser } from "@/lib/mock-data";
 
 const items = [
